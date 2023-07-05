95c4ec6ffc96d53709a893a3693f1e60dc14f5a0
cleamwafers <cleamwafers@yahoo.co.jp>
2023-07-05 22:11:15 +0900

    chapter08_演習

---
+++ Java/chapter08.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class chapter08 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.next();
+        System.out.println(text);
+    }
+}
---
+++ Java/演習.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class 演習 {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String message = scan.next();
+        System.out.println(message);
+        message = scan.next();
+        System.out.println(message);
+    }
+}
